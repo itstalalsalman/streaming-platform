00d95359de9edb7db68335e4b24095c4c1280749
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-03-31 13:16:54 +0300

    Youtube API called using RapidAPI and material UI designs allows sidebar navigation and search queries based on that

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography} from '@mui/material'
-import Sidebar from './Sidebar'
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { Sidebar, Videos} from './'
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory]);
+
   return (
     <Stack
       sx={{flexDirection: {sx: "column", md: "row"}}}
@@ -14,11 +23,21 @@ const Feed = () => {
         backgroundColor: '#000'
         }}
       >
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className='copyright' variant='body2' sx={{mt:1.5, color: '#fff'}}>
           Copyright 2024 TSX Media
         </Typography>
 
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#F31503' }} >videos</span>
+        </Typography>
+        <Videos  videos={{}} />
       </Box>
 
     </Stack>
